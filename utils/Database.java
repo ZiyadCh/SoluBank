0afee87068a545620ddb5c4c735294e91ab22cb9
ziyad <chetoziyad@gmail.com>
2026-09-22 17:09:25 +0000

    created the class that returns Connection

--- utils/Database.java
+++ utils/Database.java
@@ -1,5 +1,14 @@
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
-  private Static url = "jdbc:postgresql://localhost/solbank";
+  private static String url = "jdbc:postgresql://localhost/solbank?user=kaiser&password=kaiser";
+
+  public static Connection gConnection() throws SQLException {
+    return DriverManager.getConnection(url);
+  }
+
 }
