adf748f480eba2b8be034aa90e33ae3fdc01d887
ziyad <chetoziyad@gmail.com>
2026-09-24 11:16:55 +0000

    seperated the menus

--- Main.java
+++ Main.java
@@ -1,31 +1,70 @@
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    public static Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     while (true) {
       System.out.println("┌─────────────────────────────────────┐");
       System.out.println("│      Selectionnez une action        │");
       System.out.println("├─────────────────────────────────────┤");
-      System.out.println("│1❯ Lister les Clients                │");
-      System.out.println("│2❯ Ajouter un Client                 │");
-      System.out.println("│3❯ Modifier un Client                │");
-      System.out.println("│4❯ Supprimer un Client               │");
-      System.out.println("│5❯ Lister les Comptes                │");
-      System.out.println("│6❯ Ajouter un Compte                 │");
-      System.out.println("│7❯ Modifier un Compte                │");
-      System.out.println("│8❯ Supprimer un Compte               │");
-      System.out.println("│9❯ Lister les Transactions           │");
-      System.out.println("│10❯ Ajouter une Transaction          │");
-      System.out.println("│11❯ Modifier une Transaction         │");
-      System.out.println("│12❯ Supprimer une Transaction        │");
-      System.out.println("│13❯ Quitter                          │");
+      System.out.println("│1❯ Gestion des Clients               │");
+      System.out.println("│2❯ Gestion des Comptes               │");
+      System.out.println("│3❯ Gestion des Transactions          │");
+      System.out.println("│4❯ Quitter                           │");
       System.out.println("└─────────────────────────────────────┘");
       switch (scanner.nextLine()) {
-
+        case "1":
+          afficherMenuClients();
+          break;
+        case "2":
+          afficherMenuComptes();
+          break;
+        case "3":
+          afficherMenuTransactions();
+          break;
+        case "4":
+          System.exit(0);
       }
+      System.out.println();
     }
+  }
+
+  public static void afficherMenuClients() {
+    System.out.println("┌─────────────────────────────────────┐");
+    System.out.println("│      Gestion des Clients            │");
+    System.out.println("├─────────────────────────────────────┤");
+    System.out.println("│1❯ Lister les Clients                │");
+    System.out.println("│2❯ Ajouter un Client                 │");
+    System.out.println("│3❯ Modifier un Client                │");
+    System.out.println("│4❯ Supprimer un Client               │");
+    System.out.println("│5❯ Retour                            │");
+    System.out.println("└─────────────────────────────────────┘");
+  }
+
+  public static void afficherMenuComptes() {
+    System.out.println("┌─────────────────────────────────────┐");
+    System.out.println("│      Gestion des Comptes            │");
+    System.out.println("├─────────────────────────────────────┤");
+    System.out.println("│1❯ Lister les Comptes                │");
+    System.out.println("│2❯ Ajouter un Compte                 │");
+    System.out.println("│3❯ Modifier un Compte                │");
+    System.out.println("│4❯ Supprimer un Compte               │");
+    System.out.println("│5❯ Retour                            │");
+    System.out.println("└─────────────────────────────────────┘");
+  }
+
+  public static void afficherMenuTransactions() {
+    System.out.println("┌─────────────────────────────────────┐");
+    System.out.println("│   Gestion des Transactions          │");
+    System.out.println("├─────────────────────────────────────┤");
+    System.out.println("│1❯ Lister les Transactions           │");
+    System.out.println("│2❯ Ajouter une Transaction           │");
+    System.out.println("│3❯ Modifier une Transaction          │");
+    System.out.println("│4❯ Supprimer une Transaction         │");
+    System.out.println("│5❯ Retour                            │");
+    System.out.println("└─────────────────────────────────────┘");
   }
 }
