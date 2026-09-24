a327729aeb85b66b0ec0a34a7c6917f27ed8e65d
ziyad <chetoziyad@gmail.com>
2026-09-24 11:09:28 +0000

    made the main menu

---
+++ Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.println("┌─────────────────────────────────────┐");
+      System.out.println("│      Selectionnez une action        │");
+      System.out.println("├─────────────────────────────────────┤");
+      System.out.println("│1❯ Lister les Clients                │");
+      System.out.println("│2❯ Ajouter un Client                 │");
+      System.out.println("│3❯ Modifier un Client                │");
+      System.out.println("│4❯ Supprimer un Client               │");
+      System.out.println("│5❯ Lister les Comptes                │");
+      System.out.println("│6❯ Ajouter un Compte                 │");
+      System.out.println("│7❯ Modifier un Compte                │");
+      System.out.println("│8❯ Supprimer un Compte               │");
+      System.out.println("│9❯ Lister les Transactions           │");
+      System.out.println("│10❯ Ajouter une Transaction          │");
+      System.out.println("│11❯ Modifier une Transaction         │");
+      System.out.println("│12❯ Supprimer une Transaction        │");
+      System.out.println("│13❯ Quitter                          │");
+      System.out.println("└─────────────────────────────────────┘");
+      switch (scanner.nextLine()) {
+
+      }
+    }
+  }
+}
